bac48ef8f3463fccab807850ac01ab184db6b417
Vladimir Evterev <vladimirevterev@gmail.com>
2019-04-07 15:11:25 +0300

    + Генерация постоянных и нескольких случайных пользователей

--- src/main/java/ru/sberbank/transactionmanager/init/DBInit.java
+++ src/main/java/ru/sberbank/transactionmanager/init/DBInit.java
@@ -2,6 +2,7 @@ package ru.sberbank.transactionmanager.init;
 
 import lombok.AllArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,9 +21,12 @@ import ru.sberbank.transactionmanager.domain.repository.user.UserInfoRepository;
 import ru.sberbank.transactionmanager.domain.repository.user.UserRoleRepository;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 @AllArgsConstructor
@@ -30,6 +34,8 @@ import java.util.Random;
 public class DBInit implements ApplicationRunner {
 
     private static final String DEFAULT_PASSWORD = "123";
+
+    private static final Integer MAX_USER_COUNT = 20;
 
     BCryptPasswordEncoder passwordEncoder;
 
@@ -50,6 +56,23 @@ public class DBInit implements ApplicationRunner {
 
     private void generateDBData() {
         fillDictionaries();
+        List<UserInfo> users = generateUsers();
+        List<Account> accounts = generateAccounts(users);
+    }
+
+    private List<UserInfo> generateUsers() {
+        List<UserInfo> users = new ArrayList<>();
+        users.add(generateAdminUser());
+        users.add(generateSystemUser());
+        users.add(generateOrdinarUser());
+        int userCount = new Random().nextInt(MAX_USER_COUNT);
+        for (int i = 0; i < userCount; ++i) {
+            users.add(generateRandomUser());
+        }
+        return users;
+    }
+
+    private UserInfo generateAdminUser() {
         UserInfo adminUserInfo = UserInfo.builder()
                 .login("admin")
                 .encryptedPassword(passwordEncoder.encode(DEFAULT_PASSWORD))
@@ -62,6 +85,10 @@ public class DBInit implements ApplicationRunner {
                 adminUserInfo,
                 Collections.singletonList(roleRepository.findRoleByCode(RoleDictionary.ADMIN.getCode()))
         );
+        return adminUserInfo;
+    }
+
+    private UserInfo generateSystemUser() {
         UserInfo sysUserInfo = UserInfo.builder()
                 .login("sys")
                 .encryptedPassword(passwordEncoder.encode(DEFAULT_PASSWORD))
@@ -74,10 +101,40 @@ public class DBInit implements ApplicationRunner {
                 sysUserInfo,
                 Collections.singletonList(roleRepository.findRoleByCode(RoleDictionary.SYSTEM.getCode()))
         );
-        generateUserAccount(adminUserInfo, true);
-        generateUserAccount(adminUserInfo, false);
-        generateUserAccount(sysUserInfo, true);
-        generateUserAccount(sysUserInfo, false);
+        return sysUserInfo;
+    }
+
+    private UserInfo generateOrdinarUser() {
+        UserInfo ordinarUser = UserInfo.builder()
+                .login("user")
+                .encryptedPassword(passwordEncoder.encode(DEFAULT_PASSWORD))
+                .firstName("Пользователь")
+                .lastName("Пользователь")
+                .birthDate(LocalDate.of(1970, 1, 1))
+                .build();
+        ordinarUser = userInfoRepository.save(ordinarUser);
+        setUserRoles(
+                ordinarUser,
+                Collections.singletonList(roleRepository.findRoleByCode(RoleDictionary.USER.getCode()))
+        );
+        return ordinarUser;
+    }
+
+    private UserInfo generateRandomUser() {
+        String userName = RandomStringUtils.randomAlphabetic(15);
+        UserInfo randomUser = UserInfo.builder()
+                .login(userName.toLowerCase())
+                .encryptedPassword(passwordEncoder.encode(DEFAULT_PASSWORD))
+                .firstName(userName)
+                .lastName(userName)
+                .birthDate(LocalDate.now().minus(Period.ofDays((ThreadLocalRandom.current().nextInt(365 * 70)))))
+                .build();
+        randomUser = userInfoRepository.save(randomUser);
+        setUserRoles(
+                randomUser,
+                Collections.singletonList(roleRepository.findRoleByCode(RoleDictionary.USER.getCode()))
+        );
+        return randomUser;
     }
 
     private void fillDictionaries() {
@@ -114,12 +171,21 @@ public class DBInit implements ApplicationRunner {
         );
     }
 
-    private void generateUserAccount(UserInfo userInfo, Boolean isPrimary) {
+    private List<Account> generateAccounts(List<UserInfo> users) {
+        List<Account> accounts = new ArrayList<>();
+        for (UserInfo user : users ) {
+            accounts.add(generateUserAccount(user, true));
+            accounts.add(generateUserAccount(user, false));
+        }
+        return accounts;
+    }
+
+    private Account generateUserAccount(UserInfo userInfo, Boolean isPrimary) {
         Account account = Account.builder()
                 .balance(new Random().nextDouble() * 10000D)
                 .isPrimary(isPrimary)
                 .userInfo(userInfo)
                 .build();
-        accountRepository.save(account);
+        return accountRepository.save(account);
     }
 }
